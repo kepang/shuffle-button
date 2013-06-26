45e21e069ddec2d727fa6afa2eb98a9347e8a3da
kp <kepang.zz@gmail.com>
2013-06-26 15:07:34 -0700

    Got rid of loader. Made it a lot simpler.

--- src/com/example/milestone/MainActivity.java
+++ src/com/example/milestone/MainActivity.java
@@ -5,60 +5,86 @@ import java.util.Random;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.LoaderManager;
+import android.content.ContentResolver;
 import android.content.CursorLoader;
 import android.content.Loader;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.Menu;
 import android.widget.TextView;
 
-@SuppressLint("NewApi")
-public class MainActivity extends Activity implements LoaderManager.LoaderCallbacks<Cursor> {
+public class MainActivity extends Activity {
 
+	private final int ID_INDEX = 0;
+	private final int ARTIST_INDEX = 1;
+	private final int TITLE_INDEX = 2;
+	private final int DURATION_INDEX = 3;
+	private final int ALBUM_INDEX = 4;
+	
+	Cursor mCursor;
 	TextView tv_songTitle;
+	int songsListSize;
 	String rq_columns[] = {
+			
+			MediaStore.Audio.Media._ID,
+			MediaStore.Audio.Media.ARTIST,
 			MediaStore.Audio.Media.TITLE,
-			MediaStore.Audio.Media.DURATION
+			MediaStore.Audio.Media.DURATION,
+			MediaStore.Audio.Media.ALBUM
+			
 	};
 	
-	@SuppressLint("NewApi")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
 		tv_songTitle = (TextView) findViewById(R.id.tv_songTitle);
-		
 
-		
-		//@SuppressWarnings("deprecation")
-		/*
-		Cursor c = managedQuery(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, rq_columns, null, null, null);
-		int name = c.getColumnIndex(MediaStore.Audio.Media.TITLE);
+		ContentResolver contentResolver = getContentResolver();
+		Uri uri = android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+		mCursor = contentResolver.query(uri, rq_columns, null, null, null);
+	    songsListSize = mCursor.getCount();
 
-		c.moveToFirst();
-		tv_songTitle.setText("Title: " + c.getString(name));
-		c.moveToNext();
-		tv_songTitle.append("\n" + c.getString(name));
-		c.moveToNext();
-		tv_songTitle.append("\n" + c.getString(name));
-		c.moveToNext();
-		tv_songTitle.append("\n" + c.getString(name));
-		c.moveToNext();
-		tv_songTitle.append("\n" + c.getString(name));
-		*/
-		getLoaderManager().initLoader(0, null, this);
-		
+	
 	}
 	
 	@Override
 	protected void onResume() {
 		// TODO Auto-generated method stub
 		super.onResume();
+		if (mCursor == null) {
+		    // query failed, handle error.
+		} 
+		else if (!mCursor.moveToFirst()) {
+		    // no media on the device
+		}
+		else {
+		    //int titleColumn = mCursor.getColumnIndex(android.provider.MediaStore.Audio.Media.TITLE);
+		    //int idColumn = mCursor.getColumnIndex(android.provider.MediaStore.Audio.Media._ID);
+		    
+			mCursor.moveToPosition(new Random().nextInt(songsListSize + 1));
+			tv_songTitle.setText(mCursor.getString(ARTIST_INDEX));
+			tv_songTitle.append("\n" + mCursor.getString(TITLE_INDEX));
+			tv_songTitle.append("\n" + mCursor.getLong(ID_INDEX));
+			start();
+		}	
+
 		
-		
-		
+	}
+	
+	@Override
+	protected void onStop() {
+		// TODO Auto-generated method stub
+		super.onStop();
+	}
+	
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
 	}
 
 	@Override
@@ -68,36 +94,11 @@ public class MainActivity extends Activity implements LoaderManager.LoaderCallba
 		return true;
 	}
 	
-	
-	
 
-	@Override
-	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-		// TODO Auto-generated method stub
-		return new CursorLoader(getApplication(), MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, rq_columns, null, null, null);
-	}
-
-	@Override
-	public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
-		// TODO Auto-generated method stub
+	private void start() {
 		
 		
-		cursor.moveToFirst();
-		String test = cursor.getString(1);
-		//tv_songTitle.append("\n:\n" + cursor.getString(0));
-		/*
-		cursor.moveToNext();
-		tv_songTitle.append("\n:\n" + cursor.getString(0));
-		Random r = new Random();
-		int rand = r.nextInt(cursor.getCount());
-		cursor.moveToPosition(rand);
-		tv_songTitle.append("\n" + cursor.getString(0));
-		*/
-	}
-
-	@Override
-	public void onLoaderReset(Loader<Cursor> arg0) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
