5a9000fc6a6612c966a99079c72087ff701c6085
kp <kepang.zz@gmail.com>
2013-07-07 22:06:29 -0700

    Music Service
    Complicated.

--- src/com/example/milestone/MainActivity.java
+++ src/com/example/milestone/MainActivity.java
@@ -1,19 +1,22 @@
 package com.example.milestone;
 
-import java.io.IOException;
-import java.util.Random;
+import com.example.milestone.MpService;
+import com.example.milestone.MpService.LocalBinder;
+
 import android.app.Activity;
-import android.content.ContentResolver;
-import android.content.ContentUris;
-import android.database.Cursor;
-import android.media.AudioManager;
-import android.media.MediaPlayer;
-import android.media.MediaPlayer.OnCompletionListener;
-import android.net.Uri;
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningServiceInfo;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.PowerManager;
+import android.os.IBinder;
 import android.provider.MediaStore;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
@@ -21,28 +24,49 @@ import android.view.View.OnClickListener;
 import android.widget.ImageButton;
 import android.widget.SeekBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
-public class MainActivity extends Activity implements OnCompletionListener {
+public class MainActivity extends Activity {
 
 	private final int ID_INDEX = 0;
 	private final int ARTIST_INDEX = 1;
 	private final int TITLE_INDEX = 2;
 	private final int DURATION_INDEX = 3;
 	private final int ALBUM_INDEX = 4;
-	private final String TAG = "DEBUG";
+	private final String TAG = "Debug Activity";
 	private final int TIMER = 1000;
+	private final String ACTION_PLAY = "com.example.milestone.PLAY";
+	private final String ACTION_RESUME = "com.example.milestone.RESUME";
+	private final String BROADCAST_STR = "MP Actions";
+	private final String MSG_SONGTITLE = "msg song title";
+	private final String MSG_SONGARTIST = "msg song artist";
+	private final String MSG_SONGDURATION = "msg song duration";
+	private final String MSG_SONGINFO = "msg song info";
+	private final String MSG_SONGID = "msg song id";
+	private final String MSG_ACTION_PLAY = "msg action play";
+	private final String MSG_ACTION = "Action";
+	private final String MSG_PLAYER_READY = "msg player ready";
+	private final String MSG_PLAYER_ISPLAYING = "msg player isplaying";
+	
+	
+
+	// Service Variables
+	Intent intent;
+	MpService mService;
+	boolean mBound = false;
+	boolean isPlaying = false;
+	boolean autoPlayRequest = false;
+	boolean playerReady = false;
 	
 	ImageButton playB, nextB, previousB;
 	SeekBar seekBar;
 	Boolean playBcheck = false;
 	
-	
-	MediaPlayer mPlayer;
-	Cursor mCursor;
 	Handler mHandler;
 	TextView tv_songTitle;
 	int songsListSize;
+	Bundle bundle;
+	
+	String artist, title, duration, id;
 	
 	String rq_columns[] = {
 			
@@ -59,74 +83,129 @@ public class MainActivity extends Activity implements OnCompletionListener {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		// init UI
 		tv_songTitle = (TextView) findViewById(R.id.tv_songTitle);
-
 		mHandler = new Handler();
-		ContentResolver contentResolver = getContentResolver();
-		Uri uri = android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
-		mCursor = contentResolver.query(uri, rq_columns, null, null, null);
-	    songsListSize = mCursor.getCount();
-	    
 	    addMusicControlListenerOnButton();
+
+	    seekBar.setProgress(0);
+	    seekBar.setMax(100);
+	    
+	    // Save song info and progress for orientation change
+	    if (savedInstanceState != null) {
+	    	bundle = savedInstanceState;
+	    }
+	    else {
+	    	bundle = null;
+	    }
 	
+	}
+	
+	@Override
+		protected void onSaveInstanceState(Bundle outState) {
+			super.onSaveInstanceState(outState);
+			try {
+				Log.i(TAG, "debug parseInt id: " + id);
+				long songID = (long) Integer.parseInt(id);
+				songID = mService.mCursor.getLong(ID_INDEX);
+				int trackPosn = mService.mp.getCurrentPosition();
+				outState.putLong("ID", songID);
+				outState.putInt("Position", trackPosn);
+				Log.i(TAG, "outstate id: " + id);
+				Log.i(TAG, "mCursor.getPosition(): " + mService.mCursor.getPosition());
+				
+			}
+			catch (NullPointerException e) {
+				e.printStackTrace();
+			}
+			
+			
+		}
+	
+	@Override
+	protected void onStart() {
+		super.onStart();
 	}
 	
 	@Override
 	protected void onResume() {
 		// TODO Auto-generated method stub
 		super.onResume();
-		if (mCursor == null) {
-		    // query failed, handle error.
-			tv_songTitle.setText("Cursor query failed");
-		} 
-		else if (!mCursor.moveToFirst()) {
-		    // no media on the device
-			tv_songTitle.setText("No songs in the device");
+		
+		// Register Broadcast Receiver
+		IntentFilter iff = new IntentFilter(BROADCAST_STR);
+		LocalBroadcastManager.getInstance(this).registerReceiver(mReceiver, iff);
+
+		
+		// Start Music Player Service if it's not running
+		if (!isMpServiceRunning()) {	
+			intent = null;
+			
+			// Fresh start of the app
+			if (bundle == null) {
+				intent = new Intent(ACTION_PLAY, null, this, MpService.class);
+				autoPlayRequest = true;
+			}
+			// Orientation Change
+			else {
+				intent = new Intent(ACTION_RESUME, null, this, MpService.class);
+				intent.putExtra("ID", bundle.getLong("ID"));
+				intent.putExtra("Position", bundle.getInt("Position"));
+				intent.putExtra("Cursor", bundle.getInt("Cursor"));
+				autoPlayRequest = false;
+				Log.i(TAG, "bundle: mpid: " + bundle.getLong("ID"));
+			}
+			this.startService(intent);
+			
+			Log.i(TAG, "Start Service: " + intent.getAction());
 		}
 		else {
-			// system song ID to send to music player which song to play
-			long id = moveCursorToNextSong();
-
-			mPlayer = new MediaPlayer();
-			mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-		    mPlayer.setOnCompletionListener(this);
-
-		    selectSong(id);
-		    
-		    // init seekbar
-		    seekBar.setProgress(0);
-		    seekBar.setMax(100);
-		    updateSeekBar();
-		    autoPlay();
-		}			
+			Log.i(TAG, "Service is already running");
+		}
+		
+		// Bind activity to Music Player Service
+		if (!mBound) {
+			intent = null;
+			intent = new Intent(this, MpService.class);
+			Log.i(TAG, "Binding Service");
+			
+			// Takes time to bind to service.
+			// Go to mConnection > onServiceConnection
+			bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+		}
 	}
 	
 	@Override
 	protected void onPause() {
-		// TODO Auto-generated method stub
-		if (mPlayer != null) {
-			mPlayer.stop();
-			mPlayer.release();
-			mPlayer = null;	
-		}
 		super.onPause();
+		
+		// Unbind from music player service
+		if (mBound) {
+			Log.i(TAG, "mBound = true // from Activity-onPause");
+			unbindService(mConnection);
+			mBound = false;
+			Log.i(TAG, "Service Unbound (from Activity-onPause)");
+		}
+		
+		// Unbind Broadcast Receiver
+		LocalBroadcastManager.getInstance(this).unregisterReceiver(mReceiver);
+		
 	}
 	
 	@Override
 	protected void onStop() {
-		// TODO Auto-generated method stub
-
 		super.onStop();
+		// Stop Music Player Service if song is not playing
+		if (!mService.mp.isPlaying()) {
+			Log.i(TAG, "Call stopService()");
 
+			mService.stopService();
+		}
 	}
 	
 	@Override
 	protected void onDestroy() {
-		// TODO Auto-generated method stub
 		super.onDestroy();
-		if (mCursor != null) {
-			mCursor.close();
-		}
 
 	}
 
@@ -142,8 +221,141 @@ public class MainActivity extends Activity implements OnCompletionListener {
 	}
 	
 	/*********** PRIVATE METHODS ************/
+	
+	// Broadcast Receiver Callback
+	private BroadcastReceiver mReceiver = new BroadcastReceiver() {
+		
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			Log.i(TAG, "broadcast msg received from activity");
+			String action = intent.getStringExtra(MSG_ACTION);
+			
+			// Song info message
+			if (action == MSG_SONGINFO) {
+				artist = intent.getStringExtra(MSG_SONGARTIST);
+				title = intent.getStringExtra(MSG_SONGTITLE);
+				duration = intent.getStringExtra(MSG_SONGDURATION);
+				id = intent.getStringExtra(MSG_SONGID);
+				
+				if (intent.getBooleanExtra(MSG_PLAYER_ISPLAYING, false)) {
+					isPlaying = true;
+				}
+				else {
+					isPlaying = false;
+				}
+				
+				updateUI();
+				Log.i(TAG, "msg song info");
+
+			}
+			
+			// Player ready message (not used)
+			if (action == MSG_PLAYER_READY) {
+				if (intent.getStringExtra(MSG_PLAYER_READY) == "1") {
+					playerReady = true;
+					Log.i(TAG, "msg player ready 1");
+
+				}
+				else {
+					playerReady = false;
+					Log.i(TAG, "msg player ready 0");
+
+				}
+			}
+			
+			// Play request from Service
+			if (action == MSG_ACTION_PLAY) {
+				// Have to wait until service binding is finished
+				//autoPlayRequest = true;
+				//autoPlay();
+				mService.startMusic();
+				Log.i(TAG, "msg action play");
+			}
+			
+		}
+	};
+	
+	// Handle music player buttons, seekbar, song info, etc here
+	private void updateUI() {
+		// Update Song Info
+		Log.i(TAG, "artist:" + artist + " title:" + title +" duration" + duration);
+		tv_songTitle.setText(artist);
+		tv_songTitle.append("\n" + title);
+		
+		// Time info
+		String fillzero = "";
+		Log.i(TAG, "debug parseInt duration val:" + duration);
+		int timeInSeconds = (int) (Long.parseLong(duration) / 1000); // duration is a string
+		int minutes = timeInSeconds / 60;
+		int seconds = timeInSeconds % 60;
+		if (seconds < 10) {
+			fillzero = "0";
+		}
+		Log.i(TAG, "time:" + minutes + seconds);
+		tv_songTitle.append("\n" + minutes + ":" + fillzero + seconds);
+		
+		// Update Play button
+		if (mService != null) {
+			Log.i(TAG, "mp:" + mService.mp.toString());
+			if (mService.mp != null) {
+				isPlaying = mService.mp.isPlaying();
+			}
+		}
+
+		if (isPlaying){
+			playB.setImageResource(R.drawable.pausebtn);
+			
+		}
+		else {
+			playB.setImageResource(R.drawable.playbtn);
+		}
+		
+	}
+	
+	
+	// Check if MpService is running
+	private boolean isMpServiceRunning() {
+	    ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+	    for (RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+	        if (MpService.class.getName().equals(service.service.getClassName())) {
+	            return true;
+	        }
+	    }
+	    return false;
+	}
+	
+	// Service connection callback
+	private ServiceConnection mConnection = new ServiceConnection() {
+		
+		@Override
+		public void onServiceConnected(ComponentName name, IBinder service) {
+			// TODO Auto-generated method stub
+			LocalBinder binder = (LocalBinder) service;
+			mService = binder.getService();
+			mBound = true;
+			Log.i(TAG, "Service Binding Connected");
+			
+			updateSeekBar();
+			mService.requestSongInfoMsg();
+			if (autoPlayRequest) {
+				Log.i(TAG, "Autoplay Requested");
+				autoPlay();
+			}
+			
+						
+		}
+
+		@Override
+		public void onServiceDisconnected(ComponentName name) {
+			// TODO Auto-generated method stub
+			Log.i(TAG, "Service Binding Disconnected");
+			mBound = false;
+		}
+
+	};
+	
 	// SeekBar and song timer update thread
-	private Runnable timerThread = new Runnable() {
+	public Runnable timerThread = new Runnable() {
 
 		@Override
 		public void run() {
@@ -152,90 +364,32 @@ public class MainActivity extends Activity implements OnCompletionListener {
 			long position;
 
 			// Make sure main thread has not released the mPlayer
-			if (mPlayer != null) {
-				duration = mPlayer.getDuration();
-				position = mPlayer.getCurrentPosition();	
+			if (mService.mp != null) {
+				duration = mService.mp.getDuration();
+				position = mService.mp.getCurrentPosition();
 				int progress = (int) (100*position/duration);	
+				
 				// update seekbar position
-
 				seekBar.setProgress(progress);
 			}
 				
-
-
-			//Log.d(TAG, progress + "=(100)" + position + "/" + duration);
 			mHandler.postDelayed(timerThread, TIMER);
 		}
 		
 	};
 	
-	
-	// Play Song. INPUT: system song id
-	private void selectSong(long id) {
-
-		
-		Uri myUri = ContentUris.withAppendedId(
-				android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, id);
-		
-		try {
-			mPlayer.reset();
-			mPlayer.setDataSource(getApplicationContext(), myUri);
-			mPlayer.prepare();
-
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-		}
-		catch (IllegalStateException e) {
-			e.printStackTrace();
-		}
-		catch (IllegalArgumentException e) {
-			e.printStackTrace();
-		}
-		catch (SecurityException e) {
-			e.printStackTrace();			
-		}
-		catch (RuntimeException e) {
-			e.printStackTrace();
-		}
-		
-		// Update text
-		tv_songTitle.setText(mCursor.getString(ARTIST_INDEX));
-		tv_songTitle.append("\n" + mCursor.getString(TITLE_INDEX));
-		tv_songTitle.append("\n" + mCursor.getLong(ID_INDEX));
-		
-	}
-	
-	private long moveCursorToNextSong() {
-		// Get next song
-		mCursor.moveToPosition(new Random().nextInt(songsListSize));
-		// ID to send to music player which song to play
-		long id = mCursor.getLong(ID_INDEX);
-		
-		return id;
-	}
-	
 	private void autoPlay() {
 		playB.setImageResource(R.drawable.pausebtn);
-		playBcheck = true;
-		mPlayer.start();
-		
+		mService.startMusic();
+		isPlaying = true;
+		autoPlayRequest = false;
+		Log.i(TAG, "autoplay");
+	    //updateSeekBar();
 	}
 	
 
-	/************ LISTENERS ******************/
-	// onCompletion
-	
-	@Override
-	public void onCompletion(MediaPlayer mp) {
-		// TODO Auto-generated method stub
-		Log.i(TAG, "onCompletion");
-		selectSong(moveCursorToNextSong());
-		mPlayer.start();
-	}
-	
-	
-	/************* button listeners *************/
+	/************ LISTENERS ******************/	
+	/************* BUTTONS *************/
 	public void addMusicControlListenerOnButton(){
 		
 		// SeekBar
@@ -255,11 +409,11 @@ public class MainActivity extends Activity implements OnCompletionListener {
 				mHandler.removeCallbacks(timerThread);
 				
 				int progress = seekBar.getProgress();
-				long duration = mPlayer.getDuration();
+				long duration = mService.mp.getDuration();
 				int time = (int) (progress * duration) / 100;
-				//Log.i(TAG, progress + ":" + duration + ":" + time);
 
-				mPlayer.seekTo(time); // seekto in msec 
+				mService.mp.seekTo(time); // seekto in msec 
+				
 				updateSeekBar();
 				
 			}
@@ -283,9 +437,7 @@ public class MainActivity extends Activity implements OnCompletionListener {
 
 			@Override
 			public void onClick(View arg0) {
-				// TODO Auto-generated method stub
-				Log.i(TAG, "Previous Button Clicked");	
-				mPlayer.seekTo(0);
+				mService.playPrev();
 				updateSeekBar();
 			}		
 		});
@@ -295,14 +447,16 @@ public class MainActivity extends Activity implements OnCompletionListener {
 
 			@Override
 			public void onClick(View arg0) {
-				if(!playBcheck){
+				if(!isPlaying){
 					playB.setImageResource(R.drawable.pausebtn);
 					playBcheck = true;
-					mPlayer.start();
+					isPlaying = true;
+					mService.startMusic();
 				}else{
 					playB.setImageResource(R.drawable.playbtn);
 					playBcheck = false;
-					mPlayer.pause();
+					isPlaying = false;
+					mService.pauseMusic();
 				}	
 			}
 			
@@ -314,14 +468,16 @@ public class MainActivity extends Activity implements OnCompletionListener {
 
 			@Override
 			public void onClick(View arg0) {
-
-				mPlayer.stop();
-				long id = moveCursorToNextSong();
-				selectSong(id);
-				if (playBcheck) {
-					mPlayer.start();
+				if (mService != null) {
+					boolean isPlaying = mService.mp.isPlaying();
+					mService.playNext();
+					if (isPlaying) {
+						mService.startMusic();
+					}
 				}
+								
 			}
 		});
+		
 	}
 }
---
+++ src/com/example/milestone/MpService.java
@@ -0,0 +1,365 @@
+package com.example.milestone;
+
+
+import java.io.IOException;
+import java.util.Random;
+
+import com.example.milestone.MainActivity;
+import com.example.milestone.R;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.Intent;
+import android.database.Cursor;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
+import android.media.MediaPlayer.OnErrorListener;
+import android.media.MediaPlayer.OnPreparedListener;
+import android.net.Uri;
+import android.os.Binder;
+import android.os.IBinder;
+import android.os.PowerManager;
+import android.provider.MediaStore;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+public class MpService extends Service implements OnPreparedListener, OnErrorListener, OnCompletionListener{
+
+	private final IBinder mBinder = new LocalBinder();
+    
+	private final int ID_INDEX = 0;
+	private final int ARTIST_INDEX = 1;
+	private final int TITLE_INDEX = 2;
+	private final int DURATION_INDEX = 3;
+	private final int ALBUM_INDEX = 4;
+	private final String TAG = "Debug Svc";
+	private final int TIMER = 1000;
+	private final String ACTION_PLAY = "com.example.milestone.PLAY";
+	private final String ACTION_RESUME = "com.example.milestone.RESUME";
+	private final String BROADCAST_STR = "MP Actions";
+	private final String MSG_SONGTITLE = "msg song title";
+	private final String MSG_SONGARTIST = "msg song artist";
+	private final String MSG_SONGDURATION = "msg song duration";
+	private final String MSG_SONGINFO = "msg song info";
+	private final String MSG_SONGID = "msg song id";
+	private final String MSG_ACTION_PLAY = "msg action play";
+	private final String MSG_ACTION = "Action";
+	private final String MSG_PLAYER_READY = "msg player ready";
+	private final String MSG_PLAYER_ISPLAYING = "msg player isplaying";
+	
+	
+    MediaPlayer mp = null;
+	Cursor mCursor = null;
+	int songsListSize;
+	int seekBarProgress;
+	
+	PendingIntent pi;
+	Notification notification;
+	
+	String songTitle = "";
+	String songArtist = "";
+	String songDuration = "";
+	String songID = "";
+	String rq_columns[] = {
+			
+			MediaStore.Audio.Media._ID,
+			MediaStore.Audio.Media.ARTIST,
+			MediaStore.Audio.Media.TITLE,
+			MediaStore.Audio.Media.DURATION,
+			MediaStore.Audio.Media.ALBUM
+			
+			
+	};
+	
+	
+	
+	
+	public MpService() {
+		Log.i(TAG, "mpSvc constructor");
+
+	}
+
+    /**
+     * Class used for the client Binder.  Because we know this service always
+     * runs in the same process as its clients, we don't need to deal with IPC.
+     */
+    public class LocalBinder extends Binder {
+        MpService getService() {
+            // Return this instance of LocalService so clients can call public methods
+            return MpService.this;
+        }
+    }
+
+	@Override
+	public void onCreate() {
+		// TODO Auto-generated method stub
+		super.onCreate();
+		Log.i(TAG, "Service Created");
+		// assign the song name to songName
+		pi = PendingIntent.getActivity(getApplicationContext(), 0,
+		                new Intent(getApplicationContext(), MainActivity.class),
+		                PendingIntent.FLAG_UPDATE_CURRENT);
+		
+		notification = new Notification();
+		notification.tickerText = "ticker";
+		notification.icon = R.drawable.ic_launcher;
+		notification.flags |= Notification.FLAG_ONGOING_EVENT;
+		notification.setLatestEventInfo(getApplicationContext(), "Music Player", "Running", pi);
+		
+		//startForeground(NOTIFICATION_ID, notification);
+		startForeground(4711, notification);
+
+	}
+	
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		// TODO Auto-generated method stub
+		
+		// In case system calls service with null intent
+		if (intent == null) {
+    		Log.i(TAG, "Start Service with NULL Intent");
+    		return super.onStartCommand(intent, flags, startId);
+		}
+		
+		else {
+		
+			Log.i(TAG, "Svc Start from Service:" + intent.getAction());
+		   
+			// init mp
+	     	mp = new MediaPlayer();
+	     	mp.setWakeMode(getApplicationContext(), PowerManager.PARTIAL_WAKE_LOCK);
+			mp.setAudioStreamType(AudioManager.STREAM_MUSIC);
+		    mp.setOnCompletionListener(this);
+	        mp.setOnPreparedListener(this);
+	        
+	        // Init Cursor from MediaStore Content Provider
+	        findMusic();
+	        
+	        // Fresh load of app
+	        if (intent.getAction() == ACTION_PLAY) {
+	        	selectSong(moveCursorToNextSong());
+	        }
+	        
+	        // Orientation change while music is not playing
+	        // Restore track selection and song position
+	        if (intent.getAction() == ACTION_RESUME) {
+	        	long id = intent.getLongExtra("ID", moveCursorToNextSong());
+	        	int cPosn = 0;
+	        	
+	        	// Sync mCursor with the right song from saved bundle
+	        	mCursor.moveToFirst();
+	        	while (!mCursor.isAfterLast()) {
+	        		if (mCursor.getLong(ID_INDEX) == id) {
+	        			cPosn = mCursor.getPosition();
+	        		}
+	        		mCursor.moveToNext();
+	        	}
+	        	mCursor.moveToPosition(cPosn);
+	        	
+
+	        	selectSong(id);
+	        	mp.seekTo(intent.getIntExtra("Position", 0));
+	        	
+	        	Log.i(TAG, "selectSong ID:" + id);
+	        	Log.i(TAG, "cursor posn:" + cPosn + "id:" + mCursor.getLong(ID_INDEX));
+	        }
+	        
+	        
+	        return Service.START_NOT_STICKY;
+		}
+		
+	}
+	
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO: Return the communication channel to the service.
+		//throw new UnsupportedOperationException("Not yet implemented");
+		Log.i(TAG, "Service onBind() from Service");
+		sendMessage(MSG_SONGINFO, new String[] {songTitle, songArtist, songDuration, songID});
+		return mBinder;
+	}
+	
+	@Override
+	public boolean onUnbind(Intent intent) {
+		return super.onUnbind(intent);
+	}
+	
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		stopService();
+		Log.i(TAG, "Svc destroyed");
+
+	}
+	
+
+    /*******
+     * Media Player Listeners
+     */
+	@Override
+	public void onCompletion(MediaPlayer mp) {
+		// TODO Auto-generated method stub
+		selectSong(moveCursorToNextSong());
+		startMusic();
+	}
+
+	@Override
+	public boolean onError(MediaPlayer mp, int what, int extra) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void onPrepared(MediaPlayer mp) {
+		// TODO Auto-generated method stub
+		
+		// Player is prepared
+		sendMessage(MSG_PLAYER_READY, new String[] {"1"});
+		
+		
+	}
+	
+	/**********
+	 * Public Methods
+	 */
+	public Cursor findMusic() {
+		ContentResolver contentResolver = getContentResolver();
+		Uri uri = android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+
+		mCursor = contentResolver.query(uri, rq_columns, null, null, null);
+	    songsListSize = mCursor.getCount();
+	    return mCursor;
+	}
+	
+	public long moveCursorToNextSong() {
+		// Get next song
+		mCursor.moveToPosition(new Random().nextInt(songsListSize));
+		// ID to send to music player which song to play
+		long id = mCursor.getLong(ID_INDEX);
+		
+		return id;
+	}
+	
+	// Play Song. INPUT: system song id
+	public void selectSong(long id) {
+
+		
+		Uri myUri = ContentUris.withAppendedId(
+				android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, id);
+		
+		try {
+			mp.reset();
+			mp.setDataSource(getApplicationContext(), myUri);
+			mp.prepare();
+			//sendMessage(MSG_PLAYER_READY, new String[] {"0"});
+			//mp.prepareAsync();
+
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		catch (IllegalStateException e) {
+			e.printStackTrace();
+		}
+		catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		}
+		catch (SecurityException e) {
+			e.printStackTrace();			
+		}
+		catch (RuntimeException e) {
+			e.printStackTrace();
+		}
+		
+		// Update text
+		
+		// Send Local BCast msg to UI Thread
+		songTitle = mCursor.getString(ARTIST_INDEX);
+		songArtist = mCursor.getString(TITLE_INDEX);
+		songDuration = mCursor.getString(DURATION_INDEX);
+		songID = mCursor.getString(ID_INDEX);
+		
+		String[] msg = {songArtist, songTitle, songDuration, songID};
+		
+		sendMessage(MSG_SONGINFO, msg);
+		
+	}
+	
+	// Cleanup is here
+	public void stopService() {
+		
+		if (mCursor != null) {
+			mCursor.close();
+		}
+		if (mp != null) {
+			mp.stop();
+			mp.release();
+			mp = null;	
+		}
+		
+		stopForeground(true);
+		stopSelf();
+		Log.i(TAG, "Svc Stopped");
+	}
+
+	
+	public void startMusic() {
+		mp.start();
+	}
+	
+	public void pauseMusic() {
+		mp.pause();
+	}
+	
+	public void playNext() {
+		selectSong(moveCursorToNextSong());
+	}
+	
+	public void playPrev() {
+		mp.seekTo(0);
+	}
+	
+	public void requestSongInfoMsg() {
+		sendMessage(MSG_SONGINFO, new String[] {songArtist, songTitle, songDuration, songID});
+	}
+
+	/*******
+	 * Private Methods
+	 */
+	
+	private void sendMessage(String type, String[] msg) {
+		Intent intent = new Intent(BROADCAST_STR);
+
+		if (type == MSG_SONGINFO) {
+			intent.putExtra(MSG_ACTION, MSG_SONGINFO);
+			intent.putExtra(MSG_SONGARTIST, msg[0]);
+			intent.putExtra(MSG_SONGTITLE, msg[1]);
+			intent.putExtra(MSG_SONGDURATION, msg[2]);
+			intent.putExtra(MSG_SONGID, msg[3]);
+			intent.putExtra(MSG_PLAYER_ISPLAYING, mp.isPlaying());
+		}
+		
+		if (type == MSG_PLAYER_READY) {
+			intent.putExtra(MSG_ACTION, MSG_PLAYER_READY);
+			intent.putExtra(MSG_PLAYER_READY, msg[0]);
+		}
+	
+		LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+		Log.i(TAG, "Sent msg info from service");
+	}
+	
+	private void sendAction(String action) {
+		Intent intent = new Intent(BROADCAST_STR);
+		intent.putExtra(MSG_ACTION, MSG_ACTION_PLAY);
+		LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+		Log.i(TAG, "Sent action info from service");
+
+	}
+	
+	
+	
+}
